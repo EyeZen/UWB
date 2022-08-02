b3685cf471bed97c3d114e9ad36282d8173078a2
EyeZen <microshivam1320@gmail.com>
2022-08-02 15:23:59 +0500

    cards from dummy-data

--- card.js
+++ card.js
@@ -29,3 +29,83 @@ class Card extends HTMLElement {
     }
 }
 customElements.define('profile-card', Card);
+
+// dummy data. To be replaced by data requested from server
+const practionersData = [
+    {
+        id: 1,
+        img: './assets/card1.jpeg',
+        name: 'Dr. J.N. Tripathi',
+        degree: 'M.A. Psychology',
+        specialization: 'Clinical Depression, Dream analysis',
+        experience: 13,
+        languages: ['English', 'Hindi'],
+        availability: 'Wed',
+        about: 'Expertise: Anxiety, Relationships Sanya works with adolescents and adults. Her expertise is on working with anxiety, trauma, depression, and relationship issues. She believes in working in partnership with clients, utilizing their strengths to help them sort out their challenges and achieve their goals. She also knows how important it is to identify all aspects of an individuals life that impact their well-being including their physical health, support systems (family, friends), work or school environment, and faith/spirituality in this process.\n\nMy hobbies include swimming, cycling and reading.\n\n',
+    },
+    {
+        id: 2,
+        img: './assets/card2.png',
+        name: 'Dr. Poonam Joshi',
+        degree: 'M.Sc. Psychology',
+        specialization: 'Bipolar Disorder, Mood swings',
+        experience: 8,
+        languages: ['Hindi', 'Marathi'],
+        availability: 'Wed',
+        about: 'Expertise: Anxiety, Relationships Sanya works with adolescents and adults. Her expertise is on working with anxiety, trauma, depression, and relationship issues. She believes in working in partnership with clients, utilizing their strengths to help them sort out their challenges and achieve their goals. She also knows how important it is to identify all aspects of an individuals life that impact their well-being including their physical health, support systems (family, friends), work or school environment, and faith/spirituality in this process.\n\nMy hobbies include swimming, cycling and reading.\n\n',
+    },
+    {
+        id: 3,
+        img: './assets/card3.png',
+        name: 'Neeta Singh',
+        degree: 'M.A. Psychology',
+        specialization: 'Clinical Depression, Dream analysis',
+        experience: 12,
+        languages: ['English', 'Hindi'],
+        availability: 'Wed',
+        about: 'Expertise: Anxiety, Relationships Sanya works with adolescents and adults. Her expertise is on working with anxiety, trauma, depression, and relationship issues. She believes in working in partnership with clients, utilizing their strengths to help them sort out their challenges and achieve their goals. She also knows how important it is to identify all aspects of an individuals life that impact their well-being including their physical health, support systems (family, friends), work or school environment, and faith/spirituality in this process.\n\nMy hobbies include swimming, cycling and reading.\n\n',
+    },
+    {
+        id: 4,
+        img: './assets/card1.jpeg',
+        name: 'Dr. J.N. Tripathi',
+        degree: 'M.A. Psychology',
+        specialization: 'Clinical Depression, Dream analysis',
+        experience: 13,
+        languages: ['English', 'Hindi'],
+        availability: 'Wed',
+        about: 'Expertise: Anxiety, Relationships Sanya works with adolescents and adults. Her expertise is on working with anxiety, trauma, depression, and relationship issues. She believes in working in partnership with clients, utilizing their strengths to help them sort out their challenges and achieve their goals. She also knows how important it is to identify all aspects of an individuals life that impact their well-being including their physical health, support systems (family, friends), work or school environment, and faith/spirituality in this process.\n\nMy hobbies include swimming, cycling and reading.\n\n',
+    },
+    {
+        id: 5,
+        img: './assets/card2.png',
+        name: 'Dr. Poonam Joshi',
+        degree: 'M.Sc. Psychology',
+        specialization: 'Bipolar Disorder, Mood swings',
+        experience: 8,
+        languages: ['Hindi', 'Marathi'],
+        availability: 'Wed',
+        about: 'Expertise: Anxiety, Relationships Sanya works with adolescents and adults. Her expertise is on working with anxiety, trauma, depression, and relationship issues. She believes in working in partnership with clients, utilizing their strengths to help them sort out their challenges and achieve their goals. She also knows how important it is to identify all aspects of an individuals life that impact their well-being including their physical health, support systems (family, friends), work or school environment, and faith/spirituality in this process.\n\nMy hobbies include swimming, cycling and reading.\n\n',
+    },
+]
+
+
+function getCard(practitioner) {
+    const card = `
+        <profile-card data-img="${      practitioner.img}">
+        <span slot="name">${            practitioner.name           }</span>
+        <span slot="degree">${          practitioner.degree         }</span>
+        <span slot="specialization">${  practitioner.specialization }</span>
+        <span slot="experience">${      practitioner.experience     } years of experience</span>
+        <span slot="languages">${       practitioner.languages.join(', ')      }</span>
+        <span slot="availability">Next available on ${    practitioner.availability   }</span>
+        <span slot="about">${           practitioner.about          }</span>
+        </profile-card>
+    `
+    return card;
+}
+
+const cards = document.querySelector('.profiles');
+for(const practitioner of practionersData) {
+    cards.innerHTML += getCard(practitioner);
+}
